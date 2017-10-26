ae6f52ff0740e5be79915cdf2c275245e18781d8
liuhaifeng <1932736402@qq.com>
2017-10-26 09:09:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/cccccccccccccc/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.dell.cccccccccccccc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vp;
+    private List<Fragment> list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vp= (ViewPager) findViewById(R.id.vp);
+        list=new ArrayList<>();
+        vp.setCurrentItem(0);
+        list.add(new Fragment1());
+        vp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return list.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return 1;
+            }
+        });
+    }
+}
